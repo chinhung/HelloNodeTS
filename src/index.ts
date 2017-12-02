718eb63bd0bf2c5c7ad36227f2872399ed6be074
chinhung <chinhung101@gmail.com>
2017-12-02 23:50:14 +0800

    import expess

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-import * as http from 'http';
+import * as express from 'express';
 
 function greeter(person: string) {
     return "Hello " + person;
@@ -6,14 +6,12 @@ function greeter(person: string) {
 
 let user = "John";
 
-const server = http.createServer(function(request:http.IncomingMessage,response:http.ServerResponse):void{
-    console.log("create a server...");
-    response.writeHead(200,{'Content-Type':'text/plain'});
-    response.write(greeter(user) + ', we use typescript to develop.');
-    response.end();
+const app = express();
+
+app.get('/', (req, res) => {
+    res.send(greeter(user) + ', this is an express app built by typescript!');
 });
 
-server.listen(3000,function(){
-    console.log("Server listening on port 3000");
-    console.log("test...");
+app.listen(3000, () => {
+    console.log("Express app listening on port 3000");
 });
