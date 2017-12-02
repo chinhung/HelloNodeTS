b6acf4c69945da664fcab0791bcd1f92a3f1c757
chinhung <chinhung101@gmail.com>
2017-12-02 23:15:47 +0800

    import http module

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,19 @@
+import * as http from 'http';
+
 function greeter(person: string) {
-    return "Hello, " + person;
+    return "Hello " + person;
 }
 
 let user = "John";
 
-console.log(greeter(user));
+const server = http.createServer(function(request:http.IncomingMessage,response:http.ServerResponse):void{
+    console.log("create a server...");
+    response.writeHead(200,{'Content-Type':'text/plain'});
+    response.write(greeter(user) + ', we use typescript to develop.');
+    response.end();
+});
+
+server.listen(3000,function(){
+    console.log("Server listening on port 3000");
+    console.log("test...");
+});
